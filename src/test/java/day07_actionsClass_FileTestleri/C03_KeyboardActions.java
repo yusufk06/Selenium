0a5e7221730caa34e35103556b834b5c317da48d
“yusufk06” <yusuf-9207@outlook.com>
2023-04-26 21:59:10 +0300

    26.04

---
+++ src/test/java/day07_actionsClass_FileTestleri/C03_KeyboardActions.java
@@ -0,0 +1,56 @@
+package day07_actionsClass_FileTestleri;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C03_KeyboardActions extends TestBase {
+
+    @Test
+    public void test01(){
+        //1- https://www.facebook.com adresine gidelim
+        driver.get("https://facebook.com");
+        //2- Yeni hesap olustur butonuna basalim
+        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
+        //3- Ad, soyad, mail ve sifre kutularina deger yazalim ve kaydol tusuna basalim
+        WebElement isimKutusu= driver.findElement(By.xpath("//input[@placeholder='Adın']"));
+        Actions actions= new Actions(driver);
+        Faker faker=new Faker();
+        String fakeEmailAdress=faker.internet().emailAddress();
+        actions.click(isimKutusu)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(fakeEmailAdress)
+                .sendKeys(Keys.TAB)
+                .sendKeys(fakeEmailAdress)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("20")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Ocak")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1992")
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ENTER).perform();
+
+        //4- Kaydol tusuna basalim
+
+bekle(10);
+
+    }
+}
